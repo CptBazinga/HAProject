3b9aee6251669bcc9a32bae31288a48d442cea5f
CptBazinga <116496981+CptBazinga@users.noreply.github.com>
2023-03-31 19:54:55 +0200

    Update insert.php

--- insert.php
+++ insert.php
@@ -14,22 +14,22 @@
 		// password => empty
 		// database name => staff
 		$conn = mysqli_connect("database-1.cixaez8np9ic.eu-central-1.rds.amazonaws.com", "admin", "12345678", "TestDB");
-		
+
 		// Check connection
 		if($conn === false){
 			die("ERROR: Could not connect. "
 				. mysqli_connect_error());
 		}
-		
+
 		// Taking all 5 values from the form data(input)
 		$first_name = $_REQUEST['first_name'];
 		$last_name = $_REQUEST['last_name'];
-		
+
 		// Performing insert query execution
 		// here our table name is college
 		$sql = "INSERT INTO myTable VALUES ('$first_name',
 			'$last_name')";
-		
+
 		if(mysqli_query($conn, $sql)){
 			echo "<h3>data stored in a database successfully."
 				. " Please browse your localhost php my admin"
@@ -41,12 +41,7 @@
 			echo "ERROR: Hush! Sorry $sql. "
 				. mysqli_error($conn);
 		}
-		
+
 		// Close connection
 		mysqli_close($conn);
 		?>
-	</center>
-</body>
-
-</html>
-
