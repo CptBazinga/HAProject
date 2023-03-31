49b7264bbf104d53a2b9cb7f62e55074d5ad98e4
CptBazinga <116496981+CptBazinga@users.noreply.github.com>
2023-03-31 19:41:48 +0200

    Update insert.php

--- insert.php
+++ insert.php
@@ -22,8 +22,8 @@
 		}
 		
 		// Taking all 5 values from the form data(input)
-		$first_name = $_REQUEST['firstName'];
-		$last_name = $_REQUEST['lastName'];
+		$first_name = $_REQUEST['first_name'];
+		$last_name = $_REQUEST['last_name'];
 		
 		// Performing insert query execution
 		// here our table name is college
